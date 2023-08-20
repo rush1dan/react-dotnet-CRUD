7669fcc44022d0236c94a13752fdaee1340bf1e7
rush1dan <rushidan10@gmail.com>
2023-08-20 15:27:20 +0600

    Table of student info implemented

--- FrontEnd/ReactCRUD/src/components/StudentList.jsx
+++ FrontEnd/ReactCRUD/src/components/StudentList.jsx
@@ -2,33 +2,63 @@ import React from 'react'
 import styles from '../styles/studentlist.module.css'
 
 export const StudentList = (props) => {
+    const displayStudents = []
+
+    props.students.forEach(element => {
+        let modifiedRecord = { ...element };
+        delete modifiedRecord.id;
+        displayStudents.push(modifiedRecord);
+    });
+
+    const studentPropKeys = displayStudents.length > 0 ? Object.keys(displayStudents[0]) : [];
+
+    function formatHeadingFromKey(key) {
+        let moddedKey = key;
+        if (moddedKey.toLowerCase().includes("class"))
+        {
+            moddedKey = "class";
+        }
+        moddedKey = moddedKey.charAt(0).toUpperCase() + moddedKey.slice(1);
+        return moddedKey;
+    }
+
     return (
         <div className={styles.container}>
-            {
-                props.students.map((student, index) => {
-                    const studentProps = Object.keys(student)
-                    return (
-                        <div key={index} className={styles.studentDetails}>
-                            {
-                                studentProps.map((property, index) => {
-                                    return (
-                                        <div key={index} className={styles.property}>
-                                            <p>
-                                                <span className={styles.heading}>
-                                                    {`${property}: `}
-                                                </span>
-                                                <span className={styles.desc}>
-                                                    {student[property]}
-                                                </span>
-                                            </p>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                })
-            }
+            <table>
+                <thead>
+                    <tr>
+                        {
+                            studentPropKeys.map((key, index) => {
+                                const formattedHeading = formatHeadingFromKey(key);
+                                return (
+                                    <th key={index}>
+                                        {formattedHeading}
+                                    </th>
+                                )
+                            })
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        displayStudents.map((student, index) => {
+                            return (
+                                <tr key={index}>
+                                    {
+                                        studentPropKeys.map((key, index) => {
+                                            return (
+                                                <td>
+                                                    {student[key]}
+                                                </td>
+                                            )
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
