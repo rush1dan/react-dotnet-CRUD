81a446c9fc6cdadcf3db07934ba4e710e5ff9aa7
rush1dan <rushidan10@gmail.com>
2023-09-02 18:36:32 +0600

    Workflow setup for easy transition between development and production

--- FrontEnd/ReactCRUD/src/apicalls.js
+++ FrontEnd/ReactCRUD/src/apicalls.js
@@ -1,4 +1,5 @@
-const apiURL = "https://dot-net-crud.onrender.com/api/Students";
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+const apiURL = isDevelopment ? "http://localhost:5000/api/Students" : "https://dot-net-crud.onrender.com/api/Students";
 
 export async function get() {
     try {
